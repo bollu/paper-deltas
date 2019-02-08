23c757d4e8ff16e4b0cbb037aa7f299e7fc9a9ac
Siddharth Bhat <siddu.druid@gmail.com>
2019-02-08 12:41:23 +0530

    have working DSL/CoFree example. Now need to convert it to incremental version

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 - [Math.se question about affine groups](https://math.stackexchange.com/questions/3096752/affine-vector-spaces-with-groups)
 - [Equality on (Cont r a)](https://www.reddit.com/r/haskell/comments/ahu6jp/fun_fact_the_continuation_monad_cont_r_a_has_an/)
 - [Monads for incremental computing](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.8.3014&rep=rep1&type=pdf)
+- [Free and Cofree for interpreters](http://abailly.github.io/posts/free.html)
+- [Cofree meets Free](http://blog.sigfpe.com/2014/05/cofree-meets-free.html)
+- [Entropy as information loss](https://johncarlosbaez.wordpress.com/2011/06/02/a-characterization-of-entropy/)
 
 # To setup:
 - [latex builds with Circle](https://discuss.circleci.com/t/latex-pdf-building/668/4)
--- src/WorkingGenerics.hs
+++ src/WorkingGenerics.hs
@@ -10,10 +10,14 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE InstanceSigs #-}
 module WorkingGenerics where
 import GHC.Generics
 import GHC.Exts (Constraint)
+import Control.Monad.State.Lazy
+import Control.Monad.Identity
 
 -- type family DiffT (p :: * -> *) :: * -> *
 
@@ -27,15 +31,6 @@ data Dict :: Constraint -> * where
 instance Show (Dict a) where
   show = const "dict"
 
-
-
--- type instance DiffT ((:*:) f g) = (DiffT f) :*: (DiffT g)
-
-
--- D(xy) = xDy + yDx
--- Borrow diffgeo lingo?
--- type Derivation a b da db = Either (a, db)  (b, da)
-
 class Diff a  where
   type family Patch a :: *
   type Patch a = GPatch (Rep a) a
@@ -43,13 +38,6 @@ class Diff a  where
   diff :: a -> a -> Patch a
   default diff :: (Generic a, GDiff (Rep a), Patch a ~ (GPatch (Rep a)) a) => a -> a -> Patch a
   diff a a' = gdiff (from a) (from a')
-
--- type family DT (a :: * -> *) :: * -> *
--- type instance DT V1 = V1
--- type instance DT U1 = U1
--- type instance DT (f :*: g) = (DT f :*: DT g)
--- type instance DT (f :+: g) = DT f :+: DT g :+: f :+: g
--- type instance DT (M1 i t f) = (M1 i t (DT f))
 
 class GDiff (gen :: * -> *)  where
   type family GPatch gen :: * -> *
@@ -69,10 +57,6 @@ instance (GDiff f, GDiff g) => GDiff (f :*: g) where
   gdiff (f :*: g) (f' :*: g') = (gdiff f f') :*: (gdiff g g')
 
 
--- sums
--- instance (GDiff f fp, GDiff g gp) => GDiff (f :+: g) ((fp :+: gp) :+: (f :+: g)) where
--- instance (GDiff f fp, GDiff g gp) => GDiff (f :+: g) (fp :+: fp :+: gp) where
-
 instance (GDiff f, GDiff g) => GDiff (f :+: g)  where
   type GPatch (f :+: g) = (f :+: g :+: GPatch f :+: GPatch g)
   gdiff (L1 f) (L1 f') = R1 (R1 (L1 (gdiff f f')))
@@ -86,13 +70,6 @@ instance (GDiff f) => GDiff (M1 i t f)  where
   gdiff (M1 x) (M1 x') = M1 $ gdiff x x'
 
 
--- instance {-# OVERLAPS #-} (GDiff f fp) => GDiff (M1 i t f) fp where
---   gdiff (M1 x) (M1 x') =  gdiff x x'
--- 
--- 
--- instance {-# OVERLAPS #-} (GDiff f fp) => GDiff f (M1 i t fp) where
---   gdiff x x' = M1 $ gdiff x x'
-
 -- recursion
 instance (Diff f) => GDiff (K1 i f) where
    type GPatch (K1 i f) = (K1 i (Patch f))
@@ -101,12 +78,10 @@ instance (Diff f) => GDiff (K1 i f) where
 instance Diff () 
 instance Diff Void 
 instance (Diff a, Diff b) => Diff (a, b) 
--- instance (Diff a pa, Diff b pb) => Diff (Either a b) (Either (Either pa pb) (Either a b))
+instance (Diff a, Diff b) => Diff (Either a b)
 
 -- type instance DT Int = (NumDelta Int)
 data NumDelta a = NumDelta a deriving(Show)
---  instance Diff Int (NumDelta Int) where
---    diff a b =  NumDelta $ b - a
 
 
 instance Diff Int  where
@@ -142,12 +117,6 @@ instance Diff a => Diff [a] where
   type Patch [a] = [Patch a]
   diff (x:xs) (x':xs') = (diff x x'):(diff xs xs')
 
-
--- instance Diff [a] 
-
--- instance (Diff a) => Diff (Stream a) 
-
-
 x :: Int
 x = 3
 
@@ -169,15 +138,158 @@ l = Left 4
 r :: Either Int Int
 r = Right 10
 
--- dlr :: Four Int Int (NumDelta Int) (NumDelta Int)
--- dlr = diff l r
+dlr = diff l r
 
 
--- type instance DiffT V1  = V1
--- type instance DiffT U1 = U1
--- type instance DiffT (f :*: g) = (DiffT f) :*: (DiffT g)
--- type instance DiffT (f :+: g) = (DiffT f) :+: (DiffT g) :+: f :+: g
--- type instance DiffT 
+-- Step 2: We want nice ways to render the deltas.
+showPatch :: Diff a => Patch a -> String
+showPatch = undefined
+
+-- -- Step 3:
+-- Now that we have the diffing mechanism, we now want good incremental
+-- patching. We will probably invoke free monnad/cofree comonad
+-- to derive incremental interpreters from non incremental ones.
+
+data Free f a = Leaf a | Branch (f (Free f a)) deriving(Functor)
+
+-- f := free, g := functor 
+bindFree :: Functor g => Free g a -> (a -> Free g b) -> Free g b
+bindFree (Leaf a) a2fgb = (a2fgb a) 
+bindFree (Branch fga) a2fgb = 
+  Branch $ (`bindFree` a2fgb) <$> fga
+
+-- Does this have a better instance?
+apFree :: Functor g => Free g (a -> b) -> Free g a -> Free g b
+apFree frga2b frga = 
+  bindFree frga2b  (\a2b ->
+    bindFree frga  (\a -> Leaf $ a2b a))
+
+instance Functor f => Applicative (Free f) where
+  pure = Leaf
+  (<*>) = apFree
+
+instance Functor f => Monad (Free f) where
+ return = pure
+ (>>=) = bindFree
+
+-- cofree comonad
+data Cofree g a = Cobranch a (g (Cofree g a)) deriving(Functor)
+
+class Comonad w where
+  extract :: w a -> a
+  (=>>) :: w a -> (w a -> b) -> w b
+
+
+instance Functor g => Comonad (Cofree g) where
+  extract (Cobranch a _) = a
+  (cur@(Cobranch a g_cofree_a)) =>> cofree_a_to_b = 
+    let cur' = cofree_a_to_b cur 
+        next = (fmap (=>> cofree_a_to_b) g_cofree_a)
+    in Cobranch cur' next
+
+
+coiter :: Functor f => (a -> f a) -> a -> Cofree f a
+coiter psi a = Cobranch a (coiter psi <$> psi a)
+ 
+
+class (Functor f, Functor g) => Pairing f g where
+  pair :: (a -> b -> r) -> f a -> g b -> r
+
+instance Pairing Identity Identity where
+  pair f (Identity a) (Identity b) = f a b
+
+instance Pairing f g => Pairing (Cofree f) (Free g) where
+  pair p (Cobranch a  _ ) (Leaf x)  = p a x
+  pair p (Cobranch _ fs) (Branch gs) = pair (pair p) fs gs
+
+instance Pairing ((->) a) ((,) a) where
+  pair p f = uncurry (p . f)
+
+instance Pairing ((,) a) ((->) a) where
+  pair p f g = pair (flip p) g f
+
+
+-- Example language
+data DSLF k = Add Int (Bool -> k)
+  | Clear k
+  | Total (Int -> k) deriving(Functor)
+
+type DSL a = Free DSLF a
+
+
+liftF :: (Functor f) => f a -> Free f a
+liftF fa = Branch $ Leaf <$> fa
+
+add :: Int -> DSL Bool
+add x = liftF $ Add x  id
+
+clear :: DSL ()
+clear = liftF $ Clear ()
+
+total :: DSL Int
+total = liftF $ Total id
+
+data CoDSLF k = CoDSLF {
+  addH :: Int -> (Bool, k),
+  clearH :: k,
+  totalH :: (Int, k)
+} deriving(Functor)
+
+type Limit = Int
+type Count = Int
+type CoDSL = Cofree CoDSLF
+mkCoDSL :: Limit -> Count -> CoDSL (Limit, Count)
+mkCoDSL limit count = coiter next start
+  where
+    next w = CoDSLF (coAdd w) (coClear w) (coTotal w)
+    start = (limit, count)
+
+coClear :: (Limit, Count) -> (Limit, Count)
+coClear (limit, count) = (limit, 0)
+
+coTotal :: (Limit, Count) -> (Int, (Limit, Count))
+coTotal (limit, count) = (count, (limit, count))
+
+coAdd :: (Limit, Count) -> Int -> (Bool, (Limit, Count))
+coAdd (limit, count) x = (test, (limit, next))
+  where
+    count' = count + x                        -- 1
+    test = count' <= limit                    -- 2
+    next = if test then count' else count     -- 3
+
+
+instance Pairing CoDSLF DSLF where
+  pair f (CoDSLF a _ _) (Add x k) = pair f (a x) k
+  pair f (CoDSLF _ c _) (Clear k) = f c k
+  pair f (CoDSLF _ _ t) (Total k) = pair f t k
+
+
+double :: DSL Int
+-- or if we want to be more general:
+--   double :: Monad m => AdderT m Int
+double = do
+  -- capture the old count
+  t <- total
+  _ <- add t
+  total
+
+
+-- run with a given interpreter
+runLimit :: CoDSL a -> Int
+runLimit w = pair (\_ b -> b)  w double
+
+-- test with a given limit and start count
+testLimit :: Limit -> Count -> Int
+testLimit l c = runLimit  (mkCoDSL l c)
+
+
+
+-- TODO: incrementalize a given interpreter.
+incremental :: (Diff a, b ~ Patch a) => Cofree f a -> Cofree f b
+incremental = undefined
+
+
+
 
 main :: IO ()
 main = print "foo"
